38e596f037d0d3ce47a7d60146a80d8e3744c780
SeanHgithub <seanhadley11@gmail.com>
2025-10-17 09:34:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
 items = [
 {
-        "name": "banana",
-        "price": 0.55,
-        "department": "fruit"
-    }
+"name": "banana",
+"price": 0.55,
+"department": "fruit"},
 {
-        "name": "",
-        "price": "",
-        "department": "",
-    }
+"name": "apple",
+"price": 0.50,
+"department": "fruit"},
+{
+"name": "Table",
+"price": 150,
+"department": "Furniture"},
+{
+"name": "Couch",
+"price": 1500,
+"department": "Furniture"},
+{
+"name": "Salmon",
+"price": 8,
+"department": "Meat"},
+{
+"name": "Steak",
+"price": 10,
+"department": "Meat"},
+{
+"name": "Keyboard",
+"price": 50,
+"department": "Technology"},
+{
+"name": "PS5 Controller",
+"price": 75,
+"department": "Technology"},
+    
 ]
+ask = input("what do you want to purchase?")
